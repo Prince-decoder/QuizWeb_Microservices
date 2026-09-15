3d88c741be19aeb6253c26fecd65b859d9544c5e
ashutosh <22princeyadav214@gmail.com>
2026-09-15 18:20:44 +0530

    Adding server registry() so that different server can registor themselfs on it

---
+++ Server-Service/src/test/java/com/ashu/Server_Service/ServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashu.Server_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
