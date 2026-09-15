99c340d9950a3949910a01eb5abea4e33ad4893a
ashutosh <22princeyadav214@gmail.com>
2026-09-15 18:47:03 +0530

    Enabbling eureka server

--- Server-Service/src/main/java/com/ashu/Server_Service/ServerServiceApplication.java
+++ Server-Service/src/main/java/com/ashu/Server_Service/ServerServiceApplication.java
@@ -2,8 +2,10 @@ package com.ashu.Server_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServerServiceApplication {
 
 	public static void main(String[] args) {
